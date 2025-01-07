36acfc728eb420ffa839863296b3336d9694d417
Alex Meckes <alex.meckes@gmail.com>
2025-01-06 23:26:03 -0500

    fix: convert API endpoint to use ES modules

--- api/firecrawl/analyze.js
+++ api/firecrawl/analyze.js
@@ -1,6 +1,6 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   console.log('Request received:', {
     method: req.method,
     url: req.url,
